f8ed81b818ed3550d0b1d50311fefb1c7d222f80
Dmytro Karataiev <dkarataiev@fundrise.com>
2022-08-19 09:59:12 -0400

    feat: scaffold and top bar

--- app/src/main/java/com/fundrise/composetraining/MainActivity.kt
+++ app/src/main/java/com/fundrise/composetraining/MainActivity.kt
@@ -12,7 +12,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PizzaBuilderScreen()
+            AppTheme {
+                PizzaBuilderScreen()
+            }
         }
     }
 
--- app/src/main/java/com/fundrise/composetraining/PizzaBuilderScreen.kt
+++ app/src/main/java/com/fundrise/composetraining/PizzaBuilderScreen.kt
@@ -1,18 +1,17 @@
 package com.fundrise.composetraining
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,8 +20,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -35,30 +32,39 @@ fun PizzaBuilderScreen(
 ) {
     var pizza by rememberSaveable { mutableStateOf(Pizza()) }
 
-    Box {
-        Column(modifier = modifier) {
-            PizzaSizeDropdown(
-                pizza = pizza,
-                onEditPizza = { pizza = it },
-                modifier = modifier,
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(R.string.app_name)) }
             )
-            ToppingsList(
-                pizza = pizza,
-                onEditPizza = { pizza = it },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f, fill = true)
-            )
+        },
+        content = { padding ->
+            Box {
+                Column(modifier = modifier.padding(padding)) {
+                    PizzaSizeDropdown(
+                        pizza = pizza,
+                        onEditPizza = { pizza = it },
+                        modifier = modifier,
+                    )
+                    ToppingsList(
+                        pizza = pizza,
+                        onEditPizza = { pizza = it },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f, fill = true)
+                    )
+                }
+                OrderButton(
+                    pizza = pizza,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(dimensionResource(id = R.dimen.horizontal))
+                        .align(Alignment.BottomCenter)
+                )
+            }
         }
-        OrderButton(
-            pizza = pizza,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(dimensionResource(id = R.dimen.horizontal))
-                .align(Alignment.BottomCenter)
-        )
-    }
-
+    )
 }
 
 @Composable
